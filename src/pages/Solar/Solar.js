d5c8f634b26b4156a0ca1ca891718b1c58d58625
randy aakhus <randyaakhus@gmail.com>
2019-03-09 16:43:09 -0800

    added serve

--- src/pages/Solar/Solar.js
+++ src/pages/Solar/Solar.js
@@ -33,23 +33,13 @@ class Solar extends Component {
     payment: 0,
     data: [],
   };
-//  formatMoney = (n, c, d, t, s, i, j) => {
-//       c = isNaN(c = Math.abs(c)) ? 2 : c,
-//       d = d == undefined ? "." : d,
-//       t = t == undefined ? "," : t,
-//       s = n < 0 ? "-" : "",
-//       i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
-//       j = (j = i.length) > 3 ? j % 3 : 0;
-  
-//     return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
-//   };
   CustomToolTip = ({ payload, label, active}) =>{
     if (active && payload) {
       return (
-        <div className="custom-tooltip">
-          <p className="label">{`${label}`}</p>
-          <p className="label"> Initial : <CurrencyFormat value={payload[0].value} displayType={'text'} thousandSeparator={true} prefix={'$'}/></p>
-          <p className="label"> Return : <CurrencyFormat value={payload[1].value} displayType={'text'} thousandSeparator={true} prefix={'$'}/></p>
+        <div className="custom-tooltip" id="tooltipBackround">
+          <p className="label" id="chartcolorLabel">{`${label}`}</p>
+          <p className="label" id = "chartcolor"> Initial : <CurrencyFormat value={payload[0].value} displayType={'text'} thousandSeparator={true} prefix={'$'}/></p>
+          <p className="label" id = "chartcolor2"> Return : <CurrencyFormat value={payload[1].value} displayType={'text'} thousandSeparator={true} prefix={'$'}/></p>
         </div>
       );
     }
